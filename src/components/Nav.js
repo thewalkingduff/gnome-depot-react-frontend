2d35f78906c074c1375e19b74cbadf9a64a17c63
thewalkingduff <senorduff@gmail.com>
2022-12-22 15:09:46 -0500

    initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import logo from '../assets/gnome-depot-logo.png'
+
+export default function Nav() {
+    return (
+        <nav class="navbar">
+       <img src={logo} alt="" width={100}/>
+    </nav>
+        
+    )
+}
---
+++ src/components/Product.js
@@ -0,0 +1,9 @@
+import chubbyGnomes from '../assets/bearded-chubby.webp'
+
+export default function Product() {
+    return (
+        <div>
+            <img src={chubbyGnomes} alt="" />
+        </div>
+    )
+}
